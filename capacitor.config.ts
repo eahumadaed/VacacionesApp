a37ce16152ce3908fdf065e04690a1cb04bb365c
eahumaded <eahumada@lag.cl>
2024-09-16 05:09:07 -0300

    APK ABB

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'cl.vacaciones.app',
   appName: 'VacacionesApp',
   webDir: 'www',
   plugins: {
